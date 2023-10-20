999b4f5ea7ac819216726263f32247c1f766cb93
DEF1NE <65466003+d3f1ne@users.noreply.github.com>
2023-10-19 22:48:03 -0300

    Update templates.py

--- modules/templates/templates.py
+++ modules/templates/templates.py
@@ -2,36 +2,36 @@ from typing import List, Optional, Dict
 from pydantic import BaseModel
 
 class Info(BaseModel):
-    name: str
-    author: str
-    severity: str
+    name: str = None
+    author: str = None
+    severity: str = None
     description: Optional[str] = None
-    reference: List[str]
-    tags: List[str]
+    reference: List[str] = None
+    tags: List[str] = None
     
 class Settings(BaseModel):
     concatenate: Optional[bool] = False
 
 class Matchers(BaseModel):
-    type: Optional[str]
-    part: Optional[str]
+    type: Optional[str] = None
+    part: Optional[str] = None
     words: Optional[List[str]] = None
     indice: Optional[int] = None
     status: Optional[List[int]] = None
     value: Optional[str] = None
     
 class Keys(BaseModel):
-    subdomains: List[str]
+    subdomains: List[str] = None
 
 class Request(BaseModel):
-    method: str
-    path: str
-    headers: Optional[Dict]
+    method: str = None
+    path: str = None
+    headers: Optional[Dict] = None
     matchers: List[Matchers] = None
     
     
 class Templates(BaseModel):
-    id: str
-    info: Info
-    requests: List[Request]
+    id: str = None
+    info: Info = None
+    requests: List[Request] = None
     settings: Optional[Settings] = False
